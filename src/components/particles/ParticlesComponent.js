082bdbc4ea9cfe60c8045e5c3d8c6aca28b12c2d
ELISENWOLF <aravindprakash2001@gmail.com>
2022-06-02 20:28:04 +0530

    deployment_success

--- src/components/particles/ParticlesComponent.js
+++ src/components/particles/ParticlesComponent.js
@@ -7,23 +7,14 @@ const ParticlesComponent = (props) => {
 
   const options = useMemo(() => {
      return {
-        fpsLimit: 120,
         interactivity: {
           events: {
-            onClick: {
-              enable: true,
-              mode: "push"
-            },
             onHover: {
               enable: true,
               mode: "repulse"
             },
-            resize: true,
           },
           modes: {
-            push: {
-              quantity: 10,
-            },
             repulse: {
               distance: 100,
             }
@@ -37,9 +28,9 @@ const ParticlesComponent = (props) => {
           links: {
               color: "#a3cdef",
               enable: true,
-              distance: 70,
-              opacity: 0.5,
-              size: 4,
+              distance: 80,
+              opacity: 0.6,
+              size: 7,
           },
           collision: {
             enable: true
@@ -52,10 +43,7 @@ const ParticlesComponent = (props) => {
             value: { min: 0, max: 0.7},
           },
           size: {
-            value: { min: 1, max: 2 },
-          },
-          shape: {
-            type: "circle"
+            value: { min: 1, max: 3 },
           }
         }
       }
